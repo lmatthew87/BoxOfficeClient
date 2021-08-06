712a54399719327365af0d2837bb64bf252e8adf
lmatthew87 <81183261+lmatthew87@users.noreply.github.com>
2021-08-06 13:49:33 -0700

    parcel

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,12 +9,19 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', ImagePath: 'https://flxt.tmsimg.com/assets/p7825626_p_v10_af.jpg' },
-                { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg' },
-                { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: 'https://www.themoviedb.org/t/p/w500/ehGpN04mLJIrSnxcZBMvHeG0eDc.jpg' }
-            ],
+            movies: [],
         }
+    }
+    componentDidMount(){
+      axios.get('hhttps://pacific-thicket-04049.herokuapp.com/movies')
+        .then(response => {
+          this.setState({
+            movies: response.data
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
 
 
